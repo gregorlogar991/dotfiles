911277b9067d5d8564f74a3db1768b582f45ec5e
Gregor Logar <gregorlogar991@gmail.com>
2024-12-01 17:17:13 +0100

    Add copilotchat

---
+++ dot_config/nvim/lua/plugins/copilotchat.lua
@@ -0,0 +1,38 @@
+return {
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		keys = {
+			{
+				"<leader>aT",
+				"<cmd>CopilotChatToggle<cr>",
+				desc = "CopilotChat - Toggle",
+			},
+			{
+				"<leader>ax",
+				":CopilotChatInPlace<cr>",
+				mode = "x",
+				desc = "CopilotChat - Run in-place code",
+			},
+			{
+				"<leader>af",
+				"<cmd>CopilotChatFixDiagnostic<cr>", -- Get a fix for the diagnostic message under the cursor.
+				desc = "CopilotChat - Fix diagnostic",
+			},
+			{
+				"<leader>ar",
+				"<cmd>CopilotChatReset<cr>", -- Reset chat history and clear buffer.
+				desc = "CopilotChat - Reset chat history and clear buffer",
+			},
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	},
+}
