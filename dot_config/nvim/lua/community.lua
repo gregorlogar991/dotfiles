dfddd426f68a2d5961fd6b77a0ccb7c339de988d
Gregor Logar <gregorlogar991@gmail.com>
2025-01-26 11:18:37 +0100

    nvim edit

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -4,8 +4,8 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
-  { import = "astrocommunity.colorscheme.catppuccin" },
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.lua" },
+	-- import/override with your plugins folder
+	{ import = "astrocommunity.colorscheme.catppuccin" },
 }
