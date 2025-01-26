4c79ecbd0b0da4f6db00307b5b47e63b6c895b5f
Gregor Logar <gregorlogar991@gmail.com>
2025-01-26 11:57:15 +0100

    Update nvim, fix tmux to show status when zoomed

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -4,16 +4,20 @@
 
 ---@type LazySpec
 return {
-	"AstroNvim/astrocommunity",
-	{ import = "astrocommunity.pack.lua" },
-	-- import/override with your plugins folder
-	{ import = "astrocommunity.colorscheme.catppuccin" },
-	{ import = "astrocommunity.motion.mini-ai" },
-	{ import = "astrocommunity.diagnostics.trouble-nvim" },
-	{ import = "astrocommunity.pack.terraform" },
-	{ import = "astrocommunity.pack.markdown" },
-	{ import = "astrocommunity.pack.java" },
-	{ import = "astrocommunity.pack.bash" },
-	{ import = "astrocommunity.pack.helm" },
-	{ import = "astrocommunity.git.blame-nvim" },
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.git.blame-nvim" },
+  { import = "astrocommunity.motion.mini-ai" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.helm" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.terraform" },
 }
--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -2,43 +2,35 @@
 
 ---@type LazySpec
 return {
-	-- use mason-lspconfig to configure LSP installations
-	{
-		"williamboman/mason-lspconfig.nvim",
-		-- overrides `require("mason-lspconfig").setup(...)`
-		opts = {
-			ensure_installed = {
-				"lua_ls",
-				"dockerls",
-				"gopls",
-				"jsonls",
-				"grammarly",
-				"pylsp",
-			},
-		},
-	},
-	-- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-	{
-		"jay-babu/mason-null-ls.nvim",
-		-- overrides `require("mason-null-ls").setup(...)`
-		opts = {
-			ensure_installed = {
-				"stylua",
-				"goimports",
-				"prettier",
-				-- add more arguments for adding more null-ls sources
-			},
-		},
-	},
-	{
-		"jay-babu/mason-nvim-dap.nvim",
-		-- overrides `require("mason-nvim-dap").setup(...)`
-		opts = {
-			ensure_installed = {
-				"python",
-				"delve",
-				-- add more arguments for adding more debuggers
-			},
-		},
-	},
+  -- use mason-lspconfig to configure LSP installations
+  {
+    "williamboman/mason-lspconfig.nvim",
+    -- overrides `require("mason-lspconfig").setup(...)`
+    opts = {
+      ensure_installed = {
+        "jsonls",
+        "grammarly",
+      },
+    },
+  },
+  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+  {
+    "jay-babu/mason-null-ls.nvim",
+    -- overrides `require("mason-null-ls").setup(...)`
+    opts = {
+      ensure_installed = {
+        "prettier",
+        -- add more arguments for adding more null-ls sources
+      },
+    },
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    -- overrides `require("mason-nvim-dap").setup(...)`
+    opts = {
+      ensure_installed = {
+        -- add more arguments for adding more debuggers
+      },
+    },
+  },
 }
