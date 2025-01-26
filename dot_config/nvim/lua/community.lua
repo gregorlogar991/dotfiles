9f50951502ac97baca1977797348d1737177644f
Gregor Logar <gregorlogar991@gmail.com>
2025-01-26 13:59:05 +0100

    nvim

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -4,20 +4,21 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.diagnostics.trouble-nvim" },
-  { import = "astrocommunity.git.blame-nvim" },
-  { import = "astrocommunity.motion.mini-ai" },
-  { import = "astrocommunity.pack.bash" },
-  { import = "astrocommunity.pack.docker" },
-  { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.helm" },
-  { import = "astrocommunity.pack.java" },
-  { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.terraform" },
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.lua" },
+	-- import/override with your plugins folder
+	{ import = "astrocommunity.colorscheme.catppuccin" },
+	{ import = "astrocommunity.diagnostics.trouble-nvim" },
+	{ import = "astrocommunity.git.blame-nvim" },
+	{ import = "astrocommunity.git.gitlinker-nvim" },
+	{ import = "astrocommunity.motion.mini-ai" },
+	{ import = "astrocommunity.pack.bash" },
+	{ import = "astrocommunity.pack.docker" },
+	{ import = "astrocommunity.pack.go" },
+	{ import = "astrocommunity.pack.helm" },
+	{ import = "astrocommunity.pack.java" },
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.markdown" },
+	{ import = "astrocommunity.pack.python" },
+	{ import = "astrocommunity.pack.terraform" },
 }
